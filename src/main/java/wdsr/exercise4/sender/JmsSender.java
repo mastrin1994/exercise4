6954cd28a3cbbc81cb4e0fbb05ce4af36f559c36
Michał Wyżnikiewicz <mastrin1994@gmail.com>
2017-05-23 23:04:06 +0200

    JmsSender methods done

--- src/main/java/wdsr/exercise4/sender/JmsSender.java
+++ src/main/java/wdsr/exercise4/sender/JmsSender.java
@@ -3,43 +3,77 @@ package wdsr.exercise4.sender;
 import java.math.BigDecimal;
 import java.util.Map;
 
+import javax.jms.Connection;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MapMessage;
+import javax.jms.MessageProducer;
+import javax.jms.ObjectMessage;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import wdsr.exercise4.Order;
+
 public class JmsSender {
 	private static final Logger log = LoggerFactory.getLogger(JmsSender.class);
-	
+	private static final String LINK_TO_LOCAL_HOST = "tcp://localhost:8161";
 	private final String queueName;
 	private final String topicName;
+	private Connection connectionToBroker;
 
-	public JmsSender(final String queueName, final String topicName) {
+	public JmsSender(final String queueName, final String topicName) throws JMSException {
 		this.queueName = queueName;
 		this.topicName = topicName;
+		this.connectionToBroker = new ActiveMQConnectionFactory(LINK_TO_LOCAL_HOST).createConnection();
 	}
 
-	/**
-	 * This method creates an Order message with the given parameters and sends it as an ObjectMessage to the queue.
-	 * @param orderId ID of the product
-	 * @param product Name of the product
-	 * @param price Price of the product
-	 */
-	public void sendOrderToQueue(final int orderId, final String product, final BigDecimal price) {
-		// TODO
+	public void sendOrderToQueue(final int orderId, final String product, final BigDecimal price) throws JMSException {
+		connectionToBroker.start();
+		Session session = connectionToBroker.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		Destination destination = session.createQueue(this.queueName);
+		MessageProducer producer = session.createProducer(destination);
+
+		Order order = new Order(orderId, product, price);
+		ObjectMessage message = session.createObjectMessage(order);
+		message.setJMSType("Order");
+		message.setStringProperty("WDSR-System", "OrderProcessor");
+		producer.send(message);
+
+		session.close();
+		connectionToBroker.close();
 	}
 
-	/**
-	 * This method sends the given String to the queue as a TextMessage.
-	 * @param text String to be sent
-	 */
-	public void sendTextToQueue(String text) {
-		// TODO
+	public void sendTextToQueue(String text) throws JMSException {
+		connectionToBroker.start();
+		Session session = connectionToBroker.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		Destination destination = session.createQueue(this.queueName);
+		MessageProducer producer = session.createProducer(destination);
+
+		TextMessage message = session.createTextMessage();
+		message.setText(text);
+		producer.send(message);
+
+		session.close();
+		connectionToBroker.close();
 	}
 
-	/**
-	 * Sends key-value pairs from the given map to the topic as a MapMessage.
-	 * @param map Map of key-value pairs to be sent.
-	 */
-	public void sendMapToTopic(Map<String, String> map) {
-		// TODO
+	public void sendMapToTopic(Map<String, String> map) throws JMSException {
+		connectionToBroker.start();
+		Session session = connectionToBroker.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		Destination destination = session.createTopic(this.topicName);
+		MessageProducer producer = session.createProducer(destination);
+		MapMessage mapMessage = session.createMapMessage();
+
+		for (Map.Entry<String, String> entry : map.entrySet()) {
+			mapMessage.setString(entry.getKey(), entry.getValue());
+		}
+		producer.send(mapMessage);
+
+		session.close();
+		connectionToBroker.close();
 	}
 }
--- src/test/java/wdsr/exercise4/sender/SendTest.java
+++ src/test/java/wdsr/exercise4/sender/SendTest.java
@@ -27,75 +27,76 @@ import wdsr.exercise4.Order;
 public class SendTest {
 	static final String QUEUE_NAME = "MY.QUEUE";
 	static final String TOPIC_NAME = "MY.TOPIC";
-	
+
 	@Autowired
 	private UniversalJmsListener listener;
-	
+
 	private JmsSender service;
-	
+
 	@Before
-	public void setUp() {
+	public void setUp() throws JMSException {
 		listener.reset();
 		service = new JmsSender(QUEUE_NAME, TOPIC_NAME);
 	}
-	
-    @Test
-    @DirtiesContext
-    public void shouldSendObjectMessageToQueue() throws InterruptedException, JMSException {
-    	// given
-    	final int orderId = 3;
-    	final String product = "Rice";
-    	final BigDecimal price = BigDecimal.TEN;
-    	
-    	// when
-    	service.sendOrderToQueue(orderId, product, price);
-    	listener.waitForMessage(3, TimeUnit.SECONDS);
-    	
-    	// then
-    	Message receivedMessage = listener.getReceivedObjectMessage();
-    	assertTrue(receivedMessage instanceof ObjectMessage);
-    	Object received = ((ObjectMessage)receivedMessage).getObject();
-    	assertTrue(received instanceof Order);
-    	Order order = (Order)received;
-    	assertEquals(orderId, order.getId());
-    	assertEquals(product, order.getProduct());
-    	assertEquals(price, order.getPrice());
-    	assertEquals("Order", receivedMessage.getJMSType());
-    	assertEquals("OrderProcessor", receivedMessage.getStringProperty("WDSR-System"));
-    	assertEquals(false, listener.wasPubSub());
-    }
-    
-    @Test
-    @DirtiesContext
-    public void shouldSendTextMessageToQueue() throws InterruptedException {
-    	// given
-    	final String text = "testText";
-    	
-    	// when
-    	service.sendTextToQueue(text);
-    	listener.waitForMessage(3, TimeUnit.SECONDS);
-    	
-    	// then
-    	String received = listener.getReceivedText();
-    	assertEquals(text, received);
-    	assertEquals(false, listener.wasPubSub());
-    }
-    
-    @Test
-    @DirtiesContext
-    public void shouldSendMapMessageToTopic() throws InterruptedException {
-    	// given
-    	Map<String, String> map = new HashMap<>();
-    	map.put("key1", "val1");
-    	map.put("key2", "val2");
-    	
-    	// when
-    	service.sendMapToTopic(map);
-    	listener.waitForMessage(3, TimeUnit.SECONDS);
-    	
-    	// then
-    	@SuppressWarnings("unchecked") Map<String, String> received = listener.getReceivedMap();
-    	assertEquals(map, received);
-    	assertEquals(true, listener.wasPubSub());
-    }   
+
+	@Test
+	@DirtiesContext
+	public void shouldSendObjectMessageToQueue() throws InterruptedException, JMSException {
+		// given
+		final int orderId = 3;
+		final String product = "Rice";
+		final BigDecimal price = BigDecimal.TEN;
+
+		// when
+		service.sendOrderToQueue(orderId, product, price);
+		listener.waitForMessage(3, TimeUnit.SECONDS);
+
+		// then
+		Message receivedMessage = listener.getReceivedObjectMessage();
+		assertTrue(receivedMessage instanceof ObjectMessage);
+		Object received = ((ObjectMessage) receivedMessage).getObject();
+		assertTrue(received instanceof Order);
+		Order order = (Order) received;
+		assertEquals(orderId, order.getId());
+		assertEquals(product, order.getProduct());
+		assertEquals(price, order.getPrice());
+		assertEquals("Order", receivedMessage.getJMSType());
+		assertEquals("OrderProcessor", receivedMessage.getStringProperty("WDSR-System"));
+		assertEquals(false, listener.wasPubSub());
+	}
+
+	@Test
+	@DirtiesContext
+	public void shouldSendTextMessageToQueue() throws InterruptedException, JMSException {
+		// given
+		final String text = "testText";
+
+		// when
+		service.sendTextToQueue(text);
+		listener.waitForMessage(3, TimeUnit.SECONDS);
+
+		// then
+		String received = listener.getReceivedText();
+		assertEquals(text, received);
+		assertEquals(false, listener.wasPubSub());
+	}
+
+	@Test
+	@DirtiesContext
+	public void shouldSendMapMessageToTopic() throws InterruptedException, JMSException {
+		// given
+		Map<String, String> map = new HashMap<>();
+		map.put("key1", "val1");
+		map.put("key2", "val2");
+
+		// when
+		service.sendMapToTopic(map);
+		listener.waitForMessage(3, TimeUnit.SECONDS);
+
+		// then
+		@SuppressWarnings("unchecked")
+		Map<String, String> received = listener.getReceivedMap();
+		assertEquals(map, received);
+		assertEquals(true, listener.wasPubSub());
+	}
 }
